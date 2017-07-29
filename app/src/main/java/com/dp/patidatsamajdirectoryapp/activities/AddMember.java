b3e619d7a98cf5e4d5b1ef7a1bc302719f89ccfd
Prasheel <prasheelsoni11@gmail.com>
2017-07-30 03:40:12 +0530

    Add Member Branch

--- app/src/main/java/com/dp/patidatsamajdirectoryapp/activities/AddMember.java
+++ app/src/main/java/com/dp/patidatsamajdirectoryapp/activities/AddMember.java
@@ -23,5 +23,7 @@ public class AddMember extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_member);
+
+
     }
 }
